6dd5dc12c5ee1b8237e45530b60456daa8e22f50
David Gordon <davidgordon12@live.ca>
2024-04-06 16:48:50 -0400

    Generate processes component

---
+++ client/src/app/processes/processes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-processes',
+  standalone: true,
+  imports: [],
+  templateUrl: './processes.component.html',
+  styleUrl: './processes.component.css'
+})
+export class ProcessesComponent {
+
+}
